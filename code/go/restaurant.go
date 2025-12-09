5bd64e3c3d68eaed7d97697f063d40a064f2550c
cxrz13 <cruizmen@lion.lmu.edu>
2025-12-08 19:59:54 -0800

    go restaurant solution

--- code/go/restaurant.go
+++ code/go/restaurant.go
@@ -1,104 +1,101 @@
 package main
 
 import (
-    "log"
-	"sync"
-    "sync/atomic"
+	"log"
 	"math/rand"
-    "time"
+	"sync"
+	"sync/atomic"
+	"time"
 )
 
-
-
 type Order struct {
-    ID         uint64
-    Customer   string
-    PreparedBy string
-    Reply      chan *Order 
+	ID         uint64
+	Customer   string
+	PreparedBy string
+	Reply      chan *Order
 }
 
 var nextOrderID atomic.Uint64
 
 func do(seconds int, action ...any) {
-    log.Println(action...)
-    randomMillis := 500 * seconds + rand.Intn(500 * seconds)
-    time.Sleep(time.Duration(randomMillis) * time.Millisecond)
+	log.Println(action...)
+	randomMillis := 500*seconds + rand.Intn(500*seconds)
+	time.Sleep(time.Duration(randomMillis) * time.Millisecond)
 }
 
 func newOrder(customer string) *Order {
-    id := nextOrderID.Add(1) 
-    return &Order{
-        ID:       id,
-        Customer: customer,
-        Reply:    make(chan *Order, 1), 
-    }
+	id := nextOrderID.Add(1)
+	return &Order{
+		ID:       id,
+		Customer: customer,
+		Reply:    make(chan *Order, 1),
+	}
 }
 
 var waiter = make(chan *Order, 3)
 
 func cook(name string) {
-    log.Println(name, "starting work")
+	log.Println(name, "starting work")
 
-    // Runs forever: read from waiter, send to order.Reply
-    for order := range waiter {
-        log.Println(name, "cooking order", order.ID, "for", order.Customer)
+	// Runs forever: read from waiter, send to order.Reply
+	for order := range waiter {
 
-        time.Sleep(10 * time.Second)
+		do(10, name, "cooking order", order.ID, "for", order.Customer)
 
-        order.PreparedBy = name
+		order.PreparedBy = name
 
-        // Send the cooked order back to the customer
-        order.Reply <- order
-    }
+		// Send the cooked order back to the customer
+		order.Reply <- order
+	}
 }
 
 func customer(name string, wg *sync.WaitGroup) {
-    defer wg.Done()
+	defer wg.Done()
 
-    for mealsEaten := 0; mealsEaten < 5; {
-        order := newOrder(name)
-        log.Println(name, "placed order", order.ID)
+	for mealsEaten := 0; mealsEaten < 5; {
+		order := newOrder(name)
+		log.Println(name, "placed order", order.ID)
 
-        select {
-        case waiter <- order:
-            meal := <-order.Reply
+		select {
+		case waiter <- order:
+			meal := <-order.Reply
+			log.Println(name, "eating cooked order", meal.ID, "prepared by", meal.PreparedBy)
 
-            do(2,
-                "eating cooked order",
-            )
-            mealsEaten++
+			do(2,
+				"eating cooked order",
+			)
+			mealsEaten++
 
-        case <-time.After(7 * time.Second):
-            do(5,
-                "waiting too long, abandoning order",
-            )
-        }
-    }
+		case <-time.After(7 * time.Second):
+			do(5,
+				"waiting too long, abandoning order",
+			)
+		}
+	}
 
-    log.Println(name, "going home")
+	log.Println(name, "going home")
 }
 
-
 func main() {
-    customers := []string{
-        "Ani", "Bai", "Cat", "Dao", "Eve",
-        "Fay", "Gus", "Hua", "Iza", "Jai",
-    }
+	customers := []string{
+		"Ani", "Bai", "Cat", "Dao", "Eve",
+		"Fay", "Gus", "Hua", "Iza", "Jai",
+	}
 
-    go cook("Remy")
-    go cook("Colette")
-    go cook("Linguini")
+	go cook("Remy")
+	go cook("Colette")
+	go cook("Linguini")
 
-    var wg sync.WaitGroup
-    wg.Add(len(customers))
+	var wg sync.WaitGroup
+	wg.Add(len(customers))
 
-    for _, name := range customers {
-        go customer(name, &wg)
-    }
+	for _, name := range customers {
+		go customer(name, &wg)
+	}
 
-    wg.Wait()
+	wg.Wait()
 
-    close(waiter)
+	close(waiter)
 
-    log.Println("Restaurant closing")
+	log.Println("Restaurant closing")
 }
